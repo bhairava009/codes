479a8dfb84eaa6c09258785410bca16431e2f2b5
bhairava009 <sonumishra9809@gmail.com>
2025-07-03 15:12:03 +0530

    Updated app.py

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template #url_for
 
 app = Flask(__name__)
 
-# A decorator for registering a route with the given/default URL
+# Home route
 @app.route('/')
 def hello():
-    return "I am exicted to learn it and enjoy!"
+    return 'Hi im learning'
+# Home route
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+# Contact route
+@app.route('/contact')
+def contact():
+    # return "This is my contact"
+    return render_template('contact.html')
+
+# Address route
+@app.route('/address')
+def address():
+    # return "This is the address"
+    return render_template('address.html')
+# Bio route
+@app.route('/bio')
+def bio():
+    # return "This is my bio"/
+    return render_template('bio.html')
+
+# Dynamic route with username
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html",name=name)
+
+@app.route("/post/<int:id>",methods =['GET'])
+def post(id):
+    # return f'post ID:{id}'
+    return render_template("post.html",id=id)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
