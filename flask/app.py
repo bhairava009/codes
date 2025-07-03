c315c51109788e9009c4c6821ed573f081d28f06
bhairava009 <sonumishra9809@gmail.com>
2025-07-03 10:46:45 +0530

    Added app.py

--- flask/app.py
+++ flask/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 # A decorator for registering a route with the given/default URL
 @app.route('/')
 def hello():
-    return "Hello, I am learning Flask for the first time!"
+    return "I am exicted to learn it and enjoy!"
